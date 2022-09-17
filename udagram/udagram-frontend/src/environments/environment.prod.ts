aad1797839d3e0c35c99d527bd97ccf5796ea603
Omar Ahmed <75138543+OmarAhmed0001@users.noreply.github.com>
2022-09-17 23:42:01 +0200

    third commit

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "udagram",
-  apiHost: "http://Udagram-env.eba-vi2ue2kg.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://Udagram-env.eba-vi2ue2kg.us-east-1.elasticbeanstalk.com/api/v0"
 };
 
 /*
